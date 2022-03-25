7780ed6431025974b677b676ee05c106cfb984f5
Lulubonucci <bonucci64@gmail.com>
2022-03-25 13:18:43 +0100

    proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Boton from "./componentes/Boton";
+import Contador from "./componentes/Contador";
+import { useState } from "react";
 
 function App() {
+  const [numClick, setNumClicks] = useState(0);
+
+  const manejarClick = () => {
+    setNumClicks(numClick + 1);
+  };
+
+  const reiniciarContador = () => {
+    setNumClicks(0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="logo-contenedor"></div>
+      <div className="contenedor-principal">
+        <Contador numClick={numClick} />
+        <Boton
+          texto="Click"
+          esBotonDeClick={true}
+          manejarClick={manejarClick}
+        />
+        <Boton
+          texto="Reiniciar"
+          esBotonDeClick={false}
+          manejarClick={reiniciarContador}
+        />
+      </div>
     </div>
   );
 }
